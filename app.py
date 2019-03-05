191f1829f6d087f31296ebd751d39b0190be7687
Janie228 <29005574+Janie228@users.noreply.github.com>
2019-03-05 00:16:46 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,20 +3,25 @@ from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import Scraper as scp
 
+# Initialize flask & mongodb 
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-
+# Default home page rendering html & data pass to it
 @app.route('/')
 def index():
     mars_data = mongo.db.mars.find_one()
     return render_template('index.html', mars = mars_data)
 
+# When button was clicked on the home page, this will 
+# call the scrape function to get the updates
 @app.route('/scrape')
 def scrape():
+    # Call scrape function and get data return from it
     mars = mongo.db.mars
     data = scp.scrape_mars()
 
+    # Update mongodb with new data
     mars.update(
         {},
         data,
